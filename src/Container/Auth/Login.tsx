618a2b75c81f1ba1569ce4286040c9877fbcd00e
syd enciso <encisosydney19@gmail.com>
2024-10-23 14:41:04 +0800

    add animation and completed login form

--- src/Container/Auth/Login.tsx
+++ src/Container/Auth/Login.tsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
+
 function Login() {
+  const [formStatus, setFormStats] = useState({
+    disableInput: false,
+    isPending: false,
+  });
   return (
     <div className="login__form">
-      <div className="login__form--container">
+      <form className="login__form--container">
         <h1>Welcome back, Writer !</h1>
         <div className="login__form__input">
           <label>Email</label>
-          <input type="email" name="email" required />
+          <input
+            type="email"
+            name="email"
+            disabled={formStatus.disableInput}
+            required
+          />
         </div>
         <div className="login__form__input">
           <label>Password</label>
-          <input type="password" name="password" required />
+          <input
+            type="password"
+            name="password"
+            disabled={formStatus.disableInput}
+            required
+          />
         </div>
-        <button className="login__form--btn">Login</button>
-      </div>
+        <button type="submit" className="login__form--btn">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
