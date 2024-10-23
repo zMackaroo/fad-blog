9c2ca3d73939abba35a228e5ec44f4bdba79abb7
syd enciso <encisosydney19@gmail.com>
2024-10-23 15:18:24 +0800

    add branding

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import Logo from "../../assets/Images/icon2.png";
 
 function Header() {
   const location = useLocation();
@@ -8,7 +9,9 @@ function Header() {
     <section className="header">
       <div className="header__branding">
         <NavLink to="/">
-          <h3>{import.meta.env.VITE_COMPANY}</h3>
+          <div className="header__branding--logo">
+            <img src={Logo} />
+          </div>
         </NavLink>
         <nav className="header__nav">
           <NavLink to="/">Home</NavLink>
