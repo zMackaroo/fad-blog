d7126e5c313a97b046f2e8a8ad51c6dfd59611ba
syd enciso <encisosydney19@gmail.com>
2024-10-26 07:47:56 +0800

    resolve build errors

--- src/Container/Admin/Blogpost.tsx
+++ src/Container/Admin/Blogpost.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Blogpost() {
   return <div>Blogpost</div>;
 }
--- src/Container/Admin/Users.tsx
+++ src/Container/Admin/Users.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Users() {
   return <div>Users</div>;
 }
