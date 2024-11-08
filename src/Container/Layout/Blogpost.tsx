528e7e7154514e0fda2baad9e187345cb3909415
syd enciso <encisosydney19@gmail.com>
2024-11-09 06:47:51 +0800

    lazy load blogpost

--- src/Container/Layout/Blogpost.tsx
+++ src/Container/Layout/Blogpost.tsx
@@ -1,24 +1,40 @@
 import Parser from "html-react-parser";
+import { useEffect, useState } from "react";
 import useBlogpost from "../../Utils/Hooks/useBlogpost";
 
+import { Loading } from "../../Components";
+
 function Blogpost() {
+  const [loading, setLoading] = useState(true);
   const { content } = useBlogpost();
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <div className='blog__post__wrapper'>
-      <article className='blog__post'>
-        <div className='blog__post--title'>
-          <span>{content?.title}</span>
+    <>
+      {loading ? (
+        <Loading.Default />
+      ) : (
+        <div className='blog__post__wrapper'>
+          <article className='blog__post'>
+            <div className='blog__post--title'>
+              <span>{content?.title}</span>
+            </div>
+            <div className='blog__post--content'>
+              {content !== undefined && Parser(content?.content)}
+            </div>
+          </article>
+          {/* <div className='related__post__container'>
+            <p>Related Posts</p>
+            <div className='related__post__wrapper'>{[]}</div>
+          </div> */}
         </div>
-        <div className='blog__post--content'>
-          {content !== undefined && Parser(content?.content)}
-        </div>
-      </article>
-      {/* <div className='related__post__container'>
-        <p>Related Posts</p>
-        <div className='related__post__wrapper'>{[]}</div>
-      </div> */}
-    </div>
+      )}
+    </>
   );
 }
 
